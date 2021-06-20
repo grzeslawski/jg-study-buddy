bcd3b304c9964a660d08dd8a4fa344470c7a40df
Jarosław Grzesik <jaroslaw.grzesik94@gmail.com>
2021-06-20 22:10:32 +0200

    integration tests

--- src/components/organisms/SearchBar/SearchBar.test.js
+++ src/components/organisms/SearchBar/SearchBar.test.js
@@ -1,36 +1,18 @@
-import { render, screen, fireEvent, waitFor } from 'test-utils';
-import { setupServer } from 'msw/node';
-import { handlers } from 'mocks/handlers';
+import { render, screen, fireEvent } from 'test-utils';
 import { SearchBar } from './SearchBar';
 
-const server = setupServer(...handlers);
-
 describe('Search Bar', () => {
-  beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
-  afterAll(() => server.close());
-
   it('Renders the component', () => {
     render(<SearchBar />);
+    screen.getByText('Teacher');
     screen.getByPlaceholderText('Search');
   });
 
   it('Displays users when search phrase is matching', async () => {
     render(<SearchBar />);
     const input = screen.getByPlaceholderText('Search');
-    fireEvent.change(input, { target: { value: 'ad' } });
-    await screen.findByText(/Adam Romański/);
-  });
+    fireEvent.change(input, { target: { value: 'Low' } });
 
-  it('Hides the results when input is empty', async () => {
-    render(<SearchBar />);
-    const input = screen.getByPlaceholderText('Search');
-    fireEvent.change(input, { target: { value: 'ad' } });
-    await screen.findByText(/Adam Romański/);
-
-    fireEvent.change(input, { target: { value: '' } });
-    await waitFor(() => {
-      expect(screen.getByLabelText('results')).not.toBeVisible();
-    });
+    await screen.findByText(/Lowell/);
   });
 });
